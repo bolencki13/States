cf0869e8562440c75f1a767ff1850fa9ee4ff773
Brian Olencki <bolencki13@me.com>
2016-01-08 14:30:45 -0500

    Initial Commit

---
+++ States/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  States
+//
+//  Created by Brian Olencki on 1/8/16.
+//  Copyright © 2016 bolencki13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ States/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  States
+//
+//  Created by Brian Olencki on 1/8/16.
+//  Copyright © 2016 bolencki13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
